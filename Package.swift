a7ea0e09d6caa10f597427c7ca898c6ddaca37a5
Paul Hudson <paul.hudson@gmail.com>
2016-12-02 16:43:26 +0000

    The framework now imports as SwiftGD.

--- Package.swift
+++ Package.swift
@@ -1,7 +1,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "swiftgd",
+    name: "SwiftGD",
     dependencies: [
         .Package(url: "https://github.com/twostraws/Cgd.git", majorVersion: 0, minor: 1)
     ]
