393be3d873a43e5856210aa627fe43c2877a7051
Markus Winter <markus@wntr.me>
2018-01-16 01:41:38 +0100

    Flattens project structure and adds testing target

--- Tests/swiftgdTests/swiftgdTests.swift
+++ Tests/swiftgdTests/swiftgdTests.swift
@@ -5,7 +5,7 @@ class SwiftGDTests: XCTestCase {
     func testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
-        XCTAssertEqual(swiftgd().text, "Hello, World!")
+//        XCTAssertEqual(SwiftGD().text, "Hello, World!")
     }
 
     static var allTests: [(String, (SwiftGDTests) -> () throws -> Void)] {
