61a7fbcf44431de403d8e2643cfdf1509501f84c
Ralph Küpper <ralph.kuepper@skelpo.com>
2018-03-18 15:44:31 -0400

    Update Package.swift
    Fixed a problem with the path.

--- Package.swift
+++ Package.swift
@@ -19,7 +19,8 @@ let package = Package(
         ),
         .testTarget(
             name: "SwiftGDTests",
-            dependencies: ["SwiftGD"]
+            dependencies: ["SwiftGD"],
+            path: "Tests"
         )
     ]
 )
