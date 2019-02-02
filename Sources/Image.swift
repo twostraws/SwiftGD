062cf9c81382658a05542a6a95b9716181f18cad
Keisuke Kawamura <131e550807@gmail.com>
2019-02-02 17:18:28 +0900

    Add Image.transparent property, and Delete Image.transparent method

--- Sources/Image.swift
+++ Sources/Image.swift
@@ -21,6 +21,13 @@ public class Image {
 		return Size(width: internalImage.pointee.sx, height: internalImage.pointee.sy)
 	}
 
+	public var transparent: Bool = false {
+		didSet {
+			gdImageSaveAlpha(internalImage, transparent ? 1 : 0)
+			gdImageAlphaBlending(internalImage, transparent ? 0 : 1)
+		}
+	}
+
 	public init?(width: Int, height: Int) {
 		internalImage = gdImageCreateTrueColor(Int32(width), Int32(height))
 	}
@@ -192,11 +199,6 @@ public class Image {
 
 	public func desaturate() {
 		gdImageGrayScale(internalImage)
-	}
-
-	public func transparent(enabled: Bool) {
-		gdImageSaveAlpha(internalImage, enabled ? 1 : 0)
-		gdImageAlphaBlending(internalImage, enabled ? 0 : 1)
 	}
 
 	deinit {
