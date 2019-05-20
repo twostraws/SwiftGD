6b51d5d3aec983e7c204b4993189417bf12b1ca8
Paul Hudson <paul.hudson@gmail.com>
2019-05-20 20:50:29 +0100

    Require Swift 5.0.

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:4.0
+// swift-tools-version:5.0
 import PackageDescription
 
 let package = Package(
@@ -10,7 +10,7 @@ let package = Package(
         )
     ],
     dependencies: [
-        .package(url: "https://github.com/twostraws/Cgd.git", .upToNextMinor(from: "0.2.0"))
+        .package(url: "https://github.com/twostraws/Cgd.git", .upToNextMinor(from: "0.3.0"))
     ],
     targets: [
         .target(
